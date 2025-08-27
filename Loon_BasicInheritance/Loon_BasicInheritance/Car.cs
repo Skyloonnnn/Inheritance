c1af84aaecf079b625102d2572ad276ba3de8156
ACT-STUDENT <ACT-STUDENT@LAB3-PC27>
2025-08-27 14:50:07 +0800

    okay

---
+++ Loon_BasicInheritance/Loon_BasicInheritance/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_BasicInheritance
+{
+    internal class Car //base calss
+    {
+        //fields
+        public string carModel = "Mustang";
+
+        //method
+        public void CarInfo()
+        {
+            Console.WriteLine("Car Detail");
+        }
+    }
+}
---
+++ Loon_BasicInheritance/Loon_BasicInheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_BasicInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate and object
+            Person person = new Person();
+
+            //call the method from car class
+            person.CarInfo();
+
+            //Display the value 
+            Console.WriteLine("\nModer: " + person.carModel + "\nOwner: " + person.ownerName);
+
+            Console.ReadKey();
+        }
+    }
+}
